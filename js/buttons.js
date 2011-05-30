acf85d0a9b5a561870467d0ed0b086b691f160cb
Fonteius Condimen <fcondimen@gmail.com>
2011-05-30 23:51:44 +0200

    new position & behavior for buttons

--- js/buttons.js
+++ js/buttons.js
@@ -1,3 +1,9 @@
+function playState(state) {
+	dSeconds.style.WebkitAnimationPlayState = state;
+	seconds.style.WebkitAnimationPlayState = state;
+	minutes.style.WebkitAnimationPlayState = state;
+	hours.style.WebkitAnimationPlayState = state;
+}
 function start() {
 	dSeconds.style.WebkitAnimationName = "rotate";
 	seconds.style.WebkitAnimationName = "rotate";
@@ -7,27 +13,26 @@ function start() {
 	seconds.style.WebkitAnimationIterationCount = "infinite";
 	minutes.style.WebkitAnimationIterationCount = "infinite";
 	hours.style.WebkitAnimationIterationCount = "infinite";
-	dSeconds.style.WebkitAnimationPlayState = "running";
-	seconds.style.WebkitAnimationPlayState = "running";
-	minutes.style.WebkitAnimationPlayState = "running";
-	hours.style.WebkitAnimationPlayState = "running";
+	/*dSeconds.style.WebkitAnimationDuration = "1s";
+	seconds.style.WebkitAnimationDuration = "60s";
+	minutes.style.WebkitAnimationDuration = "3600s";
+	hours.style.WebkitAnimationDuration = "43200s";*/
+	playState("running");
 	
 }
-function pause() {
-	dSeconds.style.WebkitAnimationPlayState = "paused";
-	seconds.style.WebkitAnimationPlayState = "paused";
-	minutes.style.WebkitAnimationPlayState = "paused";
-	hours.style.WebkitAnimationPlayState = "paused";
-}
 function restart() {
-	dSeconds.style.WebkitAnimationName = "";
-	ssecondsstyle.WebkitAnimationName = "";
-	minutes.style.WebkitAnimationName = "";
-	hours.style.WebkitAnimationName = "";
-	dSeconds.style.WebkitAnimationIterationCount = "0";
-	seconds.style.WebkitAnimationIterationCount = "0";
-	minutes.style.WebkitAnimationIterationCount = "0";
-	hours.style.WebkitAnimationIterationCount = "0";
+	//pause();
+	/*;*/
+	dSeconds.style.WebkitAnimationDuration = "1s";
+	seconds.style.WebkitAnimationDuration = "1s";
+	minutes.style.WebkitAnimationDuration = "1s";
+	hours.style.WebkitAnimationDuration = "1s";
+	dSeconds.style.WebkitAnimationIterationCount = "1";
+	seconds.style.WebkitAnimationIterationCount = "1";
+	minutes.style.WebkitAnimationIterationCount = "1";
+	hours.style.WebkitAnimationIterationCount = "1";
+	playState("running")
+	//document.location.reload();
 }
 function lap() {
 }
