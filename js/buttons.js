e3624907c2ffd8524dd63b71bc0b9c7da92807d7
Juan Ortiz Zaforas <condimen@me.com>
2012-06-21 16:58:06 +0200

    1.0
    One button
    All marks are thin.
    'r' and ' ' shortcuts for refresh and start/pause

--- js/buttons.js
+++ js/buttons.js
@@ -4,8 +4,11 @@ var startState = true;
 document.onkeyup = function() {
 	if (event.keyCode == 32) {
 		playPause();
-		console.log(event.keyCode);
 	}
+	if (event.keyCode == 82) {
+		location.reload();
+	}
+	console.log(event.keyCode);
 }
 
 
@@ -20,44 +23,13 @@ function playPause() {
 
 	if (startState == true) {
 		playState("running");
-		console.log("Should be running");
+		console.log("Running");
 		// start();
 		startState = false;
 	}
 	else {
 		playState("paused");
-		console.log("Should be paused");
+		console.log("Paused");
 		startState = true;
 	}
-}
-		
-function start() {
-	dSeconds.style.WebkitAnimationName = "rotate";
-	seconds.style.WebkitAnimationName = "rotate";
-	minutes.style.WebkitAnimationName = "rotate";
-	hours.style.WebkitAnimationName = "rotate";
-	dSeconds.style.WebkitAnimationIterationCount = "infinite";
-	seconds.style.WebkitAnimationIterationCount = "infinite";
-	minutes.style.WebkitAnimationIterationCount = "infinite";
-	hours.style.WebkitAnimationIterationCount = "infinite";
-	dSeconds.style.WebkitAnimationDuration = "1s";
-	seconds.style.WebkitAnimationDuration = "60s";
-	minutes.style.WebkitAnimationDuration = "3600s";
-	hours.style.WebkitAnimationDuration = "43200s";
-}
-function restart() {
-/*	dSeconds.style.WebkitAnimationDuration = "1s";
-	seconds.style.WebkitAnimationDuration = "1s";
-	minutes.style.WebkitAnimationDuration = "1s";
-	hours.style.WebkitAnimationDuration = "1s";
-	dSeconds.style.WebkitAnimationIterationCount = "1";
-	seconds.style.WebkitAnimationIterationCount = "1";
-	minutes.style.WebkitAnimationIterationCount = "1";
-	hours.style.WebkitAnimationIterationCount = "1";
-	playState("running")*/
-	document.location.reload();
-	$("#startPause").attr("class", "start");
-}
-function lap() {
-	//to be coded
 }
